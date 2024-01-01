735b9d92b672b848bbe345440e89deae8f0bb236
ImperialCrowns <maxime.dziura@epitech.eu>
2024-01-01 02:25:58 +0100

    feat: Add dockerfile and docker-compose

--- server/src/config/config.py
+++ server/src/config/config.py
@@ -5,6 +5,12 @@ class Config(BaseSettings):
     API_URL: str
     API_KEY: str
     EMAIL: str
+    POSTGRES_USER: str
+    POSTGRES_PASSWORD: str
+    POSTGRES_HOST: str
+    DB_NAME: str
+    JWT_SECRET: str
+    DATABASE_URL: str
     model_config = SettingsConfigDict(env_file='.env')
     
 @lru_cache() 
